45bd80751c1c2838d313cb271bd7c5a22644e141
Samoloas <135927616+Samoloas@users.noreply.github.com>
2024-04-18 08:01:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def on_message(client, userdata, message):
         
 
 
-broker="broker.mqttdashboard.com"
+broker="164.92.183.45"
 port=1883
 client1= paho.Client("ELSapoPerro")
 client1.on_message = on_message
